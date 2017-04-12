a8cb0121462a0a4a3329aa750a60b72c6cc1c307
Ray Putt <rpputt@hotmail.com>
2017-04-11 19:59:17 -0700

    Finished Template walk-thru

--- app/views.py
+++ app/views.py
@@ -1,6 +1,17 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World!"
+	user = {'nickname': 'Ray'}
+
+	posts = [{'author':{'nickname': 'John'},
+			 'body': 'Nice day today'},
+			 {'author':{'nickname': 'Susan'},
+			 'body':'Yes it is'}]
+
+	return render_template('index.html',
+							title='Home',
+							user=user,
+							posts = posts)
