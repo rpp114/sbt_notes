12b882687cdf5299393b87e10b9efa42309ecc94
Ray Putt <rpputt@hotmail.com>
2017-04-13 20:51:52 -0700

    Added database config

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+from db_info import DB_CONFIG as db_config
+import os
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'password'
 
@@ -7,3 +10,9 @@ OPENID_PROVIDERS = [
     {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}
 ]
+
+SQLALCHEMY_DATABASE_URI = 'mysql://' + db_config['username'] + ':' + db_config['password'] + '@' + db_config['server'] + ':' + db_config['port'] + '/' + db_config['database']
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_MIGRATE_REPO = os.path.join(base_dir, db_repository)
