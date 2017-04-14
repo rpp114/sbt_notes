43d9a294274cd57f8c874e444ba9bed6cdb8f614
Ray Putt <rpputt@hotmail.com>
2017-04-13 20:52:17 -0700

    Added DB Config

---
+++ db_info.py
@@ -0,0 +1,5 @@
+DB_CONFIG = {'username': 'titlow_notes_adm',
+            'password': 'Sp072384#',
+            'database': 'titlow_notes_app',
+            'port': '3306',
+            'server': 'dallas139.arvixeshared.com'}
