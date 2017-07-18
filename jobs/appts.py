8531b82aa260afdb138f7c7b73831551a50d8f27
Ray Putt <rpputt@hotmail.com>
2017-07-18 11:04:57 -0700

    pulled appts in through api.  Working on putting them into the DB.  Created Job Management file to manage all jobs.

--- jobs/appts.py
+++ jobs/appts.py
@@ -1,5 +1,5 @@
 
-import datetime, pytz, httplib2, json, sys, os
+import httplib2, json, sys, os
 
 from apiclient import discovery
 from oauth2client import client
@@ -9,27 +9,33 @@ from sqlalchemy import and_
 
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
 
-from app import db, models
+from app import db #, models
 
 
-def get_therapist_appts():
+def get_therapist_appts(therapist, start_time, end_time):
 
-    therapists = models.Therapist.query.filter(and_(models.Therapist.user.has(status='active'), models.Therapist.status=='active'))
+    ''' Needs dates use standard datetime.datetime python format, and Therapist Object from the query return of models.Therapist'''
 
-    for therapist in therapists:
-        credentials = client.OAuth2Credentials.from_json(therapist.user.calendar_credetials)
+    credentials = client.OAuth2Credentials.from_json(json.loads(therapist.user.calendar_credentials))
 
-    return 'I\'m Done'
+    if credentials.access_token_expired:
+        credentials.refresh(httplib2.Http())
+        therapist.user.calendar_credentials = json.dumps(credentials.to_json())
+        db.session.add(therapist)
+        db.session.commit()
 
 
+    http_auth = credentials.authorize(httplib2.Http())
+    service = discovery.build('calendar', 'v3', http=http_auth)
+
+    # calendar = service.calendars().get(calendarId='primary').execute()
 
 
-therapists = get_therapist_appts()
+    eventsResults = service.events().list(calendarId='primary', timeMin=start_time.isoformat(), timeMax=end_time.isoformat(), orderBy='startTime', singleEvents=True).execute()
 
-print(therapists)
+    return eventsResults.get('items', [])
 
-    # therapist_creds = json.loads(therapist[2])
-    #
-    # print(therapist[0], credentials.access_token_expired)
 
-    # print(girl[0], girl[1],tokens)
+def enter_appts_to_db(appts, therapist):
+
+    for appt in appts:
---
+++ jobs/job_controller.py
@@ -0,0 +1,34 @@
+import datetime, pytz, httplib2, json, sys, os
+
+from apiclient import discovery
+from oauth2client import client
+from sqlalchemy import and_
+
+# add system directory to pull in app & models
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
+
+from app import db, models
+
+from appts import get_therapist_appts
+
+
+
+d = datetime.datetime.now()
+
+today = d.replace(tzinfo=pytz.timezone('US/Pacific'))
+
+tomorrow = today + datetime.timedelta(days=1)
+
+# ids: Sarah = 5 Claire = 6
+therapists = models.Therapist.query.filter(and_(models.Therapist.user.has(status='active'), models.Therapist.status=='active', models.Therapist.id == 6))
+
+
+for t in therapists:
+    print(t.user.first_name)
+    appts = get_therapist_appts(t, today, tomorrow)
+    print(len(appts))
+    for appt in appts:
+        print('appt summary: ', appt.get('summary', None), ' | Appt Status: ', appt.get('status', None), ' | Appt StartTime: ', appt['start']['dateTime'])
+
+    print(appts[0])
