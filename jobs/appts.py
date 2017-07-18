36a8e087f53bb3ae50f6c4fa976f588a88664949
Ray Putt <rpputt@hotmail.com>
2017-07-18 09:21:46 -0700

    adding job to pull appts from google calendar and feed into appts db

--- jobs/appts.py
+++ jobs/appts.py
@@ -1,27 +1,35 @@
 
-import datetime, pytz, httplib2, json
+import datetime, pytz, httplib2, json, sys, os
 
 from apiclient import discovery
 from oauth2client import client
 from sqlalchemy import and_
 
+# add system directory to pull in app & models
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
+
 from app import db, models
 
 
-def get_therapists():
+def get_therapist_appts():
 
-    therapists = [(t.user.first_name, t.user.last_name, t.user.calendar_credentials) for t in models.Therapist.query.filter(and_(models.Therapist.user.has(status='active'), models.Therapist.status=='active'))]
+    therapists = models.Therapist.query.filter(and_(models.Therapist.user.has(status='active'), models.Therapist.status=='active'))
 
-    return therapists
+    for therapist in therapists:
+        credentials = client.OAuth2Credentials.from_json(therapist.user.calendar_credetials)
+
+    return 'I\'m Done'
 
 
 
 
-girls = get_therapists()
+therapists = get_therapist_appts()
 
-for girl in girls:
-    credentials = client.OAuth2Credentials.from_json(json.loads(girls[2]))
+print(therapists)
 
-    print(credentials)
+    # therapist_creds = json.loads(therapist[2])
+    #
+    # print(therapist[0], credentials.access_token_expired)
 
     # print(girl[0], girl[1],tokens)
