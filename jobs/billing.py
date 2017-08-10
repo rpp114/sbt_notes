f3d1437608599a20a3ccedca2139487c7722d7ae
Ray Putt <rpputt@hotmail.com>
2017-08-09 22:23:01 -0700

    moved output of gather appts to array of appts to use Building XML more regularly.  Still building XML file

--- jobs/billing.py
+++ jobs/billing.py
@@ -22,62 +22,63 @@ def gather_appts(regional_center, start_time, end_time):
 
 def build_appt_xml(appts):
 
-    '''Takes the output obejct of gather_appts() format:{'user_id':[appt[0], appt[1]]} and will write XML file to static docs directory.'''
-
+    '''Takes an array of Appt Objects and will write XML file to static docs directory.'''
     appts_by_client = {}
 
     for appt in appts:
         appts_by_client[appt.client.id] = appts_by_client.get(appt.client.id, {})
-        appts_by_client[appt.client.id][appt.appt_type.name] = appts_by_client[appt.client.id].get(appt.appt_type.id, [])
+        appts_by_client[appt.client.id][appt.appt_type.id] = appts_by_client[appt.client.id].get(appt.appt_type.id, [])
         appts_by_client[appt.client.id][appt.appt_type.id].append(appt)
-        appt_days.append(appt.start_datetime.day)
-
-    print('appts_by_client', appts_by_client)
-
-
 
     tai = Element('TAI')
 
-    for client_id in appts:
-        appt_days = []
+    for client_id in appts_by_client:
         # Grab Auth to check dates and Max Visits
         # If auth is not valid Skip appt writing and leave billed.
         # If More appts then Auth Max pop last appt and leave it off.
         client = models.Client.query.get(client_id)
         client_auths = client.auths
-        current_month = appts[client_id][0].start_datetime.replace(day=1)
+        current_month = appts[0].start_datetime.replace(day=1)
+        current_auth = None
         for auth in client_auths:
             if current_month >= auth.auth_start_date and current_month <= auth.auth_end_date:
                 current_auth = auth
-            else:
-                # Add note about Auth Needed
-                continue
-        current_auth = client_auths.filter(and_(current_month >= client.auths.auth_start_date, current_month <= client.auths.auth_end_date))
 
-        for appt_type_id in appts[client_id]:
+        if current_auth == None:
+            continue
+
+        for appt_type_id in appts_by_client[client_id]:
             appt_type = models.ApptType.query.get(appt_type_id)
+            appt_days = [d.start_datetime.day for d in appts_by_client[client_id][appt_type_id]]
 
             invoice_data = SubElement(tai, 'invoicedata')
+
             RecType = SubElement(invoice_data, 'RecType')
             RecType.text = 'D'
             RCID = SubElement(invoice_data, 'RCID')
-            RCID.text = client.regional_center.rc_id
+            RCID.text = str(client.regional_center.rc_id)
             ATTN = SubElement(invoice_data, 'AttnOnlyFlag')
             UCI = SubElement(invoice_data, 'UCI')
-            UCI.text = client.uci_id
+            UCI.text = str(client.uci_id)
             lastname = SubElement(invoice_data, 'lastname')
             lastname.text = client.last_name.upper()
             firstname = SubElement(invoice_data, 'firstname')
             firstname.text = client.first_name.upper()
             auth_number = SubElement(invoice_data, 'AuthNumber')
-            auth_number.text = current_auth.auth_id
+            auth_number.text = str(current_auth.auth_id)
             svc_code = SubElement(invoice_data, 'SVCCode')
             svc_code.text = appt_type.service_type_code
-            svcs_code = SubElement(invoice_date, 'SVCSCode')
+            svcs_code = SubElement(invoice_data, 'SVCSCode')
             svcs_code.text = appt_type.service_type_code
-            
+            svc_mn_yr = SubElement(invoice_data, 'SVCMnYr')
+            svc_mn_yr.text = current_month.strftime('%Y-%m-%d')
+            industry_type = SubElement(invoice_data, 'IndustryType')
+            wage_amt = SubElement(invoice_data, 'WageAmt')
+            wage_type = SubElement(invoice_data, 'WageType')
 
-    print(tostring(tai))
+            print(appt_days)
+
+    # print(tostring(tai, encoding='utf8', method='xml'))
 
 
 
