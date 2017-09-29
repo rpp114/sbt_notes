a3d729e7919211dfbfb5aa2dd033fcf98b96a1ff
Ray Putt <rpputt@hotmail.com>
2017-09-29 08:30:13 -0700

    fixed bug in appt grabber

--- jobs/hourly_job_controller.py
+++ jobs/hourly_job_controller.py
@@ -23,25 +23,33 @@ import emails
 
 def get_new_appts():
 
-    min_times = db.session\
-                .query(models.ClientAppt.therapist_id,func.max(models.ClientAppt.end_datetime))\
-                .join(models.Therapist).join(models.User)\
-                .filter(models.Therapist.status == 'active', models.User.status == 'active')\
-                .group_by(models.ClientAppt.therapist_id)\
-                .all()
-
     pdt = pytz.timezone("America/Los_Angeles")
     est = pytz.timezone("America/New_York")
     max_time = pdt.normalize(est.localize(datetime.datetime.now()))
 
-    for t in min_times:
-        min_time = pdt.localize(t[1])
-        therapist = models.Therapist.query.get(t[0])
+    therapists = models.Therapist.query.filter(models.Therapist.status == 'active', models.Therapist.user.has(status = 'active')).all()
+
+    min_times = dict(db.session\
+                .query(models.ClientAppt.therapist_id,func.max(models.ClientAppt.end_datetime))\
+                .join(models.Therapist).join(models.User)\
+                .filter(models.Therapist.status == 'active', models.User.status == 'active')\
+                .group_by(models.ClientAppt.therapist_id)\
+                .all())
+
+    for therapist in therapists:
+
+        min_time = min_times.get(therapist.id, False)
+
+        if min_time:
+            min_time = pdt.localize(min_time)
+        else:
+             min_time = max_time - datetime.timedelta(days=1)
+
         new_appts = enter_appts_to_db(therapist, min_time, max_time)
         messages = emails.get_appt_messages(new_appts)
         emails.send_emails(therapist.user.email, messages)
 
-        print('Sent %s %s emails' % (therapist.user.first_name, len(new_appts)))
+        print('Sent %s %s emails for appts from %s to %s' % (therapist.user.first_name, len(new_appts),min_time.strftime('%b %d, %Y %H:%M %p'), max_time.strftime('%b %d, %Y %H:%M %p')))
 
 
 #execute jobs (No, Not Steve!!)
