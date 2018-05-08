4c9f4c43246efa57eeaa5e421c6d4ff2bdfb7f75
Ray Putt <rpputt@hotmail.com>
2018-05-07 22:10:56 -0700

    changing evals and appt pulling script

--- jobs/appts.py
+++ jobs/appts.py
@@ -126,11 +126,11 @@ def enter_appts_to_db(therapist, start_time, end_time):
         appointment_type='treatment' if ((end_time - start_time).seconds/60) == 60 else 'evaluation'
 
 
-        appt_type_id_result = db.session.query(models.ApptType.id)\
+        appt_type_id = db.session.query(models.ApptType.id)\
                     .join(models.RegionalCenter)\
                     .filter(models.RegionalCenter.appt_reference_name == rc_from_appt,\
                             models.RegionalCenter.company_id == therapist.user.company_id,\
-                            func.lower(models.ApptType.name) == appointment_type).first()
+                            func.lower(models.ApptType.name) == appointment_type).first()[0]
 
 
         new_appt = models.ClientAppt(
