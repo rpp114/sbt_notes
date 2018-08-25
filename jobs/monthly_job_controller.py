e50cf96feb72d1aa68a6ebdb80f80829e0431f07
ray <ray@home-pc.socal.rr.com>
2018-08-25 13:13:53 -0700

    updated monthly jobs to match prod

--- jobs/monthly_job_controller.py
+++ jobs/monthly_job_controller.py
@@ -1,3 +1,33 @@
+<<<<<<< HEAD
+#!/home/ray/notes/notes/bin/python3.5
+
+import datetime, sys, os, pytz
+
+# add system directory to pull in app & models
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
+
+from app import db, models
+
+def need_new_appts():
+
+    pdt = pytz.timezone('America/Los_Angeles')
+
+    today = pdt.localize(datetime.datetime.now())
+
+    auths_need_appts = models.ClientAuth.query.filter(models.ClientAuth.status == 'active', models.ClientAuth.monthly_visits <= 2, models.ClientAuth.is_eval_only == 0, models.ClientAuth.auth_start_date <= today, models.ClientAuth.auth_end_date >= today, models.ClientAuth.client.has(status = 'active')).all()
+
+    for auth in auths_need_appts:
+        client = auth.client
+        client.needs_appt_scheduled = 1
+        db.session.add(client)
+
+    db.session.commit()
+
+#    print('need appts for {} clients'.format(len(auths_need_appts)))
+
+need_new_appts()
+=======
 
 #!/home/ray/notes/notes/bin/python3.5
 
@@ -27,3 +57,4 @@ def need_new_appts():
 #    print('need appts for {} clients'.format(len(auths_need_appts)))
 
 need_new_appts()
+>>>>>>> c8d44480bed56c74a22763ed5823ed5179690f0a
