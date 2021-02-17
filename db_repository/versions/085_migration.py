fc8c86fe5810c1a2399815da45bd4b4af3abaeaa
ray <rpputt@hotmail.com>
2021-02-17 13:03:54 -0800

    include db upgrade

---
+++ db_repository/versions/085_migration.py
@@ -0,0 +1,36 @@
+from sqlalchemy import *
+from migrate import *
+
+
+from migrate.changeset import schema
+pre_meta = MetaData()
+post_meta = MetaData()
+client_appt = Table('client_appt', post_meta,
+    Column('id', INTEGER, primary_key=True, nullable=False),
+    Column('client_id', INTEGER),
+    Column('therapist_id', INTEGER),
+    Column('start_datetime', DATETIME),
+    Column('end_datetime', DATETIME),
+    Column('appointment_type', VARCHAR(length=15)),
+    Column('appt_type_id', INTEGER),
+    Column('cancelled', SMALLINT, default=ColumnDefault(0)),
+    Column('location', VARCHAR(length=255)),
+    Column('mileage', INTEGER, default=ColumnDefault(0)),
+    Column('billing_xml_id', INTEGER),
+    Column('created_date', DATETIME),
+)
+
+
+def upgrade(migrate_engine):
+    # Upgrade operations go here. Don't create your own engine; bind
+    # migrate_engine to your metadata
+    pre_meta.bind = migrate_engine
+    post_meta.bind = migrate_engine
+    post_meta.tables['client_appt'].columns['created_date'].create()
+
+
+def downgrade(migrate_engine):
+    # Operations to reverse the above upgrade go here.
+    pre_meta.bind = migrate_engine
+    post_meta.bind = migrate_engine
+    post_meta.tables['client_appt'].columns['created_date'].drop()
