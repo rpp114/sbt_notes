0a257e075d2eba913351224e381ce4f820167e5f
ray <rpputt@hotmail.com>
2021-07-12 19:19:31 -0700

    added yearly job to update auths annually.

---
+++ jobs/yearly_job_controller.py
@@ -0,0 +1,52 @@
+#!/home/ray/notes/notes/bin/python
+
+import datetime as dt, sys, os
+from sqlalchemy import String
+from sqlalchemy.sql import func
+from sqlalchemy.sql.expression import cast
+
+# add system directory to pull in app & models
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
+
+from app import db, models, app
+
+
+def create_new_year_auths():
+    
+    cur_date = dt.datetime.now().replace(day=1)
+    
+    cur_year = cur_date.strftime('%y')
+    
+    new_year = str(int(cur_year) + 1)
+    
+    auths_to_update = models.ClientAuth.query.filter(models.ClientAuth.auth_end_date >= cur_date,
+                                                     models.ClientAuth.is_eval_only == 0,
+                                                     models.ClientAuth.status == 'active',
+                                                     func.left(cast(models.ClientAuth.auth_id, String),2) == cur_year,
+                                                     ).all()
+    
+    for old_auth in auths_to_update:
+        
+        if old_auth.client.status == 'active':
+            new_auth_no = int(new_year + str(old_auth.auth_id)[2:]) 
+            
+            new_auth = models.ClientAuth(auth_id = new_auth_no,
+                                        status = 'active',
+                                        auth_end_date = old_auth.auth_end_date,
+                                        auth_start_date = old_auth.auth_start_date,
+                                        client_id = old_auth.client_id,
+                                        monthly_visits = old_auth.monthly_visits)
+            
+            old_auth.status =  'inactive'                
+            
+            db.session.add(old_auth)
+            db.session.add(new_auth)
+            print('Inactivated auth_no: {}. Added new auth_no: {}.'.format(old_auth.auth_id, new_auth.auth_id))
+                    
+    db.session.commit()
+
+
+
+with app.app_context():
+    create_new_year_auths()
