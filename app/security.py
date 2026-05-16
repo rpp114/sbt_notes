67c22b4c9c61f73b407b7251820040b2c85aa58b
Ray <rpputt@hotmail.com>
2026-05-16 14:09:21 -0600

    extending the session timeout

--- app/security.py
+++
@@ -1,6 +0,0 @@
-
-import app
-from flask_security import Security, SQLAlchemyUserDatastore
-
-user_datastore = SQLAlchemyUserDatastore(app.db, app.models.User, app.models.Role)
-security = Security(app, user_datastore)
